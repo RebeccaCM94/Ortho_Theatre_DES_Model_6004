8c2252da899ae36f53b78700853bd6a6d85490e3
RebeccaCM94 <rebecca.marshall@uhcw.nhs.uk>
2024-11-27 12:08:27 +0000

    INIT: add model class - resources

--- DES_Model_Project_v1.1.py
+++ DES_Model_Project_v1.1.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd
 
 #Class to store global parameter values. We don't create an instance of this
-#class - we just refer to the class blueprint itself to access the numbers inside.
+#class - we refer to the class blueprint itself to access the numbers inside.
 
 #Variables calculated from 1 Jan 2023 - 31 Oct 2024
 #emergency procedures - do the same for elective procedures
@@ -12,7 +12,7 @@ class g:
     avg_emergency_ops_per_day = 7
     number_ops_per_emergency_case = 1.3 #excl 2 very big outliers
     #number_of_nurses = 1 ##work these out
-    #number_of_doctors = 2 ##work these out
+    number_of_doctors = 2 ##work these out
     number_of_sessions_per_day = 7 #avg 8 hrs a day - elective + emerg
     prob_need_emerg_proc = 0.716 
     mean_op_duration_emerg = 145 
@@ -29,3 +29,34 @@ class Patient:
         self.id = p_id
         self.op_duration_time = 0
 
+#Model class
+class Model:
+    # Constructor to set up the model for a run.  We pass in a run number when
+    # we create a new model.
+    def __init__(self, run_number):
+        # Create a SimPy environment in which everything will live
+        self.env = simpy.Environment()
+
+    # Create a patient counter (which we'll use as a patient ID)
+        self.patient_counter = 0
+
+    # Create our resources
+        self.sessions = simpy.Resource(self.env, 
+            capacity=g.number_of_sessions_per_day)
+        self.doctor = simpy.Resource(self.env, capacity=g.number_of_doctors) 
+
+    # Store the passed in run number
+        self.run_number = run_number
+
+    # Create a new Pandas DataFrame that will store some results against
+        # the patient ID (which we'll use as the index).
+        self.results_df = pd.DataFrame()
+        self.results_df["Patient ID"] = [1]
+        self.results_df["Duration of Op"] = [0.0]
+        self.results_df.set_index("Patient ID", inplace=True)
+
+    # Create an attribute to store the mean queuing times across this run of
+        # the model
+        self.mean_duration_time = 0
+
+        
